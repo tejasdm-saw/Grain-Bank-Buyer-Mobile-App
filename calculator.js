8ea4231d70741c3274dc718d2f8cf804de3e2707
Tejas DM <tejasdm1999@gmail.com>
2026-08-21 11:41:20 +0530

    Adding a new code to check validations in P1-1

--- calculator.js
+++ calculator.js
@@ -21,6 +21,6 @@ function divide(a, b) {
 
 // Example usage
 console.log("Addition:", add(2, 5));         // 8
-console.log("Subtract:", subtract(5, 3)); // 2
+console.log("Subtraction:", subtract(5, 3)); // 2
 console.log("Multiply:", multiply(5, 3)); // 15
 console.log("Divide:", divide(5, 3));     // 1.666...
