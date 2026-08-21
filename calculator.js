c8cca3181b77c9c15e45a22c51046eabc2026f4c
Tejas DM <tejasdm1999@gmail.com>
2026-08-21 19:06:29 +0530

    Testing commit no 1 for P1-9 and P2-2

--- calculator.js
+++ calculator.js
@@ -22,5 +22,5 @@ function divide(a, b) {
 // Example usage
 console.log("Addition:", add(2, 15));         // 8
 console.log("Subtract:", subtract(5, 3)); // 2
-console.log("Multiply:", multiply(5, 3)); // 15
+console.log("Multiply:", multiply(50, 3)); // 15
 console.log("Divide:", divide(5, 3));     // 1.666...
