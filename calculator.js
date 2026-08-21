334674e59d592e4b64ac0eb3878766f2a61eba10
Tejas DM <tejasdm1999@gmail.com>
2026-08-21 11:29:43 +0530

    Fixed a data issue in P1-6

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function divide(a, b) {
 }
 
 // Example usage
-console.log("Add:", add(5, 5));         // 8
+console.log("Add:", add(1, 5));         // 8
 console.log("Subtract:", subtract(5, 3)); // 2
 console.log("Multiply:", multiply(5, 3)); // 15
 console.log("Divide:", divide(5, 3));     // 1.666...
