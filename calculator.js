0b504a5f3c1fb128eff9522304e3afd671570407
Tejas DM <tejasdm1999@gmail.com>
2026-08-28 09:17:12 +0530

    Testing commit no 1 for P6-4 and P6-5 in QA env

--- calculator.js
+++ calculator.js
@@ -22,5 +22,5 @@ function divide(a, b) {
 // Example usage
 console.log("Addition:", add(20, 20));         // 8
 console.log("Subtract:", subtract(20, 20)); // 2
-console.log("Multiply:", multiply(15, 15)); // 15
+console.log("Multiply:", multiply(15, 20)); // 15
 console.log("Divide:", divide(20, 20));     // 1.666...
