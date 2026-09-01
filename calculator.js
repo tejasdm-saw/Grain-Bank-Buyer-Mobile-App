56af13aad79b12142353736b9d7ed707c74df5f3
Tejas DM <tejasdm1999@gmail.com>
2026-09-01 16:30:50 +0530

    Testing commit no 1 for P4-4 in dev env

--- calculator.js
+++ calculator.js
@@ -21,6 +21,6 @@ function divide(a, b) {
 
 // Example usage
 console.log("Addition:", add(25, 25));         // 8
-console.log("Subtract:", subtract(20, 20)); // 2
+console.log("Subtract:", subtract(25, 20)); // 2
 console.log("Multiply:", multiply(20, 20)); // 15
 console.log("Divide:", divide(20, 20));     // 1.666...
